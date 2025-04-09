3e3f6bbd14c9d488329fb57aaa83c96189d3c120
MUHAMMAD ZAKY AL-FARIZY <zakyfaryz@student.telkomuniversity.ac.id>
2025-04-09 08:01:34 +0700

    comit terakhir

--- modul7_kelompok_1/Program.cs
+++ modul7_kelompok_1/Program.cs
@@ -3,4 +3,15 @@
 using System.Text.Json;
 using System;
 using System.IO;
+using modul7_kelompok_1;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Json 1");
+        DataMahasiswa_103022330051.ReadJSON();
+        Console.WriteLine("Json 2");
+        TeamMembers_103022330051.ReadJSON();
+    }
+}
